b804e494d54392b8fb5bf2ffe33b6736a233c5ae
Adam Sokołowski <adam.sokolowski@gmail.com>
2020-03-31 20:11:23 +0200

    feat(all): add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: ['/coverage/', '/dist/', '/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      functions: 90
+    }
+  }
+};
