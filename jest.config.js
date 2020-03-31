3575bb594814e3fe72a077b3f180ee45ec2087be
Adam Sokołowski <adam.sokolowski@gmail.com>
2020-03-31 20:32:17 +0200

    feat(all): adds editor config, husky, eslint, .md

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: { '^.+\\.tsx?$': 'ts-jest' },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testPathIgnorePatterns: ['/coverage/', '/dist/', '/node_modules/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: 'ts',
   collectCoverage: true,
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageThreshold: {
