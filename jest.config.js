104a02d305a07db4167cecfff3ce70d66ac7c067
Adam Sokołowski <adam.sokolowski@gmail.com>
2020-08-08 18:13:17 +0200

    fix(jest): update module extensions list

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: { '^.+\\.tsx?$': 'ts-jest' },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testPathIgnorePatterns: ['/coverage/', '/dist/', '/node_modules/'],
-  moduleFileExtensions: 'ts',
+  moduleFileExtensions: ['ts, js'],
   collectCoverage: true,
   coverageReporters: ['json-summary', 'text-summary'],
   coveragePathIgnorePatterns: ['/node_modules/'],
